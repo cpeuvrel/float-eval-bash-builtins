38df4603ec8f7581f930daeeb8d51174e9eea4a2
Corentin Peuvrel <corentin.peuvrel@gmail.com>
2015-12-16 00:12:45 +0100

    FEATURE/MEDIUM: float_eval: Add bunch of helpers for stack management
    Init:
      void init_stack(stack_t* s) : init a stack_t
    
    I/O:
      void print_stack(stack_t s) : print the content of a stack_t (assuming values
                                    are char*
      void print_mpfr_stack(stack_t s) : print the content of a stack_t (assuming
                                    values are mpfr_t*
    
    Cleanup:
      void clear(stack_t s) : free the memory of a stack when content can be
                              directly freed
      void clear_mpfr(stack_t s) : free the memory of a stack containing mpfr_t*
    
    Stack specific:
      void push(void* val, stack_t* s) : push val on the top of s
      void* pop(stack_t* s) : pop a value from s and return it

--- float_eval.c
+++ float_eval.c
@@ -18,6 +18,14 @@ char* op_prio[16][8] = {
     {""}
 };
 
+static void init_stack(stack_t* s);
+static void print_stack(stack_t s);
+static void print_mpfr_stack(stack_t s);
+static void clear(stack_t s);
+static void clear_mpfr(stack_t s);
+static void push(void* val, stack_t* s);
+static void* pop(stack_t* s);
+
 int main(int argc, const char *argv[])
 {
     if (argc < 2) {
@@ -51,6 +59,9 @@ void float_eval(mpfr_t *res, char* str)
     mpfr_t *val_num, *ptr;
     int type, parenthesis = 0;
 
+    init_stack(&stack_v);
+    init_stack(&stack_o);
+
     while ((val = tokenify(&str, &type, &parenthesis))) {
         if (type == FLOAT_EVAL_NUM) {
             val_num = malloc(sizeof(mpfr_t));
@@ -68,6 +79,98 @@ void float_eval(mpfr_t *res, char* str)
     ptr = pop(&stack_v);
     mpfr_set(*res, *ptr, MPFR_RNDN);
 
+    clear(stack_o);
+    clear_mpfr(stack_v);
+
     mpfr_clear(*ptr);
     free(ptr);
+}
+
+/*
+ * Initialize a stack_t
+ */
+static void init_stack(stack_t* s)
+{
+    s->size = FLOAT_EVAL_STACK_BEG_SIZE;
+    s->pos = 0;
+    s->stack = malloc(s->size * sizeof(void*));
+    s->stack[0] = NULL;
+}
+
+/*
+ * Print the whole stack given
+ */
+static void print_stack(stack_t s)
+{
+    int i;
+
+    for (i = 0; s.stack[i]; i++) {
+        fprintf(stderr, "%s\n", (char*)s.stack[i]);
+    }
+}
+
+/*
+ * Print the whole stack given (mpfr_t* values)
+ */
+static void print_mpfr_stack(stack_t s)
+{
+    int i;
+
+    for (i = 0; s.stack[i]; i++) {
+        mpfr_out_str(stderr, 10, 3, s.stack[i], MPFR_RNDN);
+        putc('\n', stderr);
+    }
+}
+
+/*
+ * Free the memory declared on the heap for the stack given
+ */
+static void clear(stack_t s)
+{
+    for (; s.pos >= 0; s.pos--) {
+        free(s.stack[s.pos]);
+    }
+    free(s.stack);
+}
+
+/*
+ * Free the memory (mpfr_t*) declared on the heap for the stack given
+ */
+static void clear_mpfr(stack_t s)
+{
+    for (; s.pos > 0; s.pos--) {
+        mpfr_clear(s.stack[s.pos]);
+    }
+    free(s.stack);
+}
+
+/*
+ * Push a value to the stack
+ */
+static void push(void* val, stack_t* s)
+{
+    if (s->pos == s->size) {
+        s->size *= 2;
+        s->stack = realloc(s->stack, s->size * sizeof(void*));
+    }
+
+    // Push the value
+    s->stack[s->pos] = val;
+    s->pos++;
+
+    s->stack[s->pos] = NULL;
+}
+
+/*
+ * Pop a value from the stack and return it
+ */
+static void* pop(stack_t* s)
+{
+    void *res;
+
+    s->pos--;
+    res = s->stack[s->pos];
+    s->stack[s->pos] = NULL;
+
+    return res;
 }
